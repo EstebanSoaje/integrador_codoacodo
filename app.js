3aeed017caac25b477921e4026f583757edadf1b
Esteban Soaje Ferreres <esferreres@gmail.com>
2023-11-20 17:55:35 -0300

    controladores y rutas node

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const mainRoutes = require('./src/routes/mainRoutes.js');
 
-app.get('/ping', (req, res) => res.send('pong'));
+/* Requerimos la dependencia*/
+require('dotenv').config();
+/* Leemos la constante*/
+const PORT = process.env.PORT;
 
-app.listen(port, () => console.log("Servidor coriendo en http://localhost:" + port));
 
+// --- Comunica el sitio con la carpeta public enviando todos los archivos estáticos ---
 app.use(express.static('public'));
+app.use('/', mainRoutes);
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+const methodOverride = require('method-override');
+// Override para habilitar los métodos PUT y DELETE
+app.use(methodOverride('_method'));
+
+// Middleware para manejar el error 404
+app.use((req, res, next) => {
+    res.status(404).send('Recurso no encontrado');
+});
+
+
+app.listen(PORT, () => console.log(`Servidor coriendo en http://localhost:${PORT}`));
