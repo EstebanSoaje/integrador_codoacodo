a087a296b0d9c76832c54889bf38418be52870fb
Esteban Soaje Ferreres <esferreres@gmail.com>
2023-12-10 19:26:01 -0300

    funcionalidad para traer todos los items desde json y un item por id

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,15 +1,19 @@
 
 
-const funkos = require('../../funkos.json')
+const items = require('../../funkos.json')
 const shopControllers = {
     shop: (req, res) => res.send("Página de Shop"),
     itemID: (req, res) => { //path-to-regexp
         const {id} = req.params
+        const item = items.find(item => item.id === id)
+        if(item) return res.json(item)
+    
+        res.status(404).json({message: 'Item no encontrado'})
     },
     itemADD: (req, res) => res.send("Agregar item actual"),
     shopCartGet:  (req, res) => res.send("Route para traer carrito"),
     shopCartPOST:  (req, res) => res.send("Route para post de carrito"),
-    funkos: (req, res) => res.json(funkos)
+    items: (req, res) => res.json(items)
 }
 
 module.exports = shopControllers;
--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const shopControllers = require('../controllers/shopController.js');
+
+const items = require('../../funkos.json')
+
 /* SHOP ROUTES */
 router.get('/shop', shopControllers.shop);
 router.get('/shop/item/:id', shopControllers.itemID);
@@ -9,7 +12,8 @@ router.get('/shop/cart', shopControllers.shopCartGet);
 router.post('/shop/cart', shopControllers.shopCartPOST);
 
 //traer todos los funkos del json/db
-router.get('/shop/items', shopControllers.funkos)
+router.get('/shop/items', shopControllers.items)
+router.get('/shop/items/:id', shopControllers.itemID)
 
 module.exports = router;
 
